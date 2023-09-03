8a06a2930a2e997ccd0f38d964593ccfe89ad91d
Eduardo Goes <eduardo.paulog@gmail.com>
2023-09-03 19:12:26 -0300

    lint: ignore some linting rules from hard-to-cirvument rules (without refactoring, at least)

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,10 +26,16 @@ module.exports = {
     'import/extensions': 0,
     // TODO: Create dedicated logger & get rid of this ESLint disable
     'no-console': 0,
+    // No way to not await in loop in a card game... or is there?
     'no-await-in-loop': 0,
+    // TODO: Refactor class methods in order to not reassign params
     'no-param-reassign': [0, { props: false }],
+    /*
+      TODO: Check if we should refactor the while loops in main.ts & giving-cards.ts
+      in order to not ignore this rule
+    */
     'no-continue': 0,
-    // TODO: Transform the violations from this rule into helper functions
+    // TODO: Transform the violations for this rule into helper functions
     'class-methods-use-this': 0,
   },
 };
